14fb478d4e3caf5cfe2cfcf7c3a9fb3f3b72cd78
Gerardo Cid <gerardo.cid@ipsoft.com>
2017-05-17 21:29:21 -0400

    Create React component example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'semantic-ui-react';
+import styles from './../semantic/dist/semantic.min.css';
+
+/* Comment like this */
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={styles}>
+          Semantic UI theme Builder
+          <hr/>
+
+
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+
       </div>
     );
   }
